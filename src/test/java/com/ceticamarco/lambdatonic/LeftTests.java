b2084b6df9ea5dfde0d3aa75631a902966fe1da1
Marco Cetica <email@marcocetica.com>
2024-09-18 10:47:29 +0200

    Added unit tests for functor laws

--- src/test/java/com/ceticamarco/lambdatonic/LeftTests.java
+++ src/test/java/com/ceticamarco/lambdatonic/LeftTests.java
@@ -3,6 +3,8 @@ package com.ceticamarco.lambdatonic;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.function.Function;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -47,5 +49,29 @@ public class LeftTests {
             Throwable::getMessage,
             _ -> "Undefined variable"
         ), "Undefined variable");
+    }
+
+    @Test
+    public void testLeftFunctorIdentityMorphism() {
+        // Applying the map function with the identity function,
+        // should not change data type structure
+        Either<Error, Integer> result = this.numEither.map(Function.identity());
+
+        assertEquals(this.numEither, result);
+    }
+
+    @Test
+    public void testLeftFunctorCompositionMorphism() {
+        // map ( f . g ) == map f . map g
+        Function<Integer, Integer> f = x -> x + 1;
+        Function<Integer, Integer> g = x -> x * 2;
+
+        // Evaluate map x . map g
+        Either<Error, Integer> gMapped = this.numEither.map(g);
+        Either<Error, Integer> FGMapped = gMapped.map(f);
+        // Evaluate map ( f . g )
+        Either<Error, Integer> composition = this.numEither.map(f.compose(g));
+
+        assertEquals(composition, FGMapped);
     }
 }
